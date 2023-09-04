47fb2d2eb6249a9b1e9872543fa0cadaedc86e68
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-09-04 08:00:34 -0700

    Update Cargio Token.sol

--- Cargio-token-contract/Cargio Token.sol
+++ Cargio-token-contract/Cargio Token.sol
@@ -17,13 +17,6 @@ interface IBEP20 {
 
   function transfer(address recipient, uint256 amount) external returns (bool);
 
-  /**
-   * @dev Returns the remaining number of tokens that `spender` will be
-   * allowed to spend on behalf of `owner` through {transferFrom}. This is
-   * zero by default.
-   *
-   * This value changes when {approve} or {transferFrom} are called.
-   */
   function allowance(address _owner, address spender) external view returns (uint256);
 
   /**
