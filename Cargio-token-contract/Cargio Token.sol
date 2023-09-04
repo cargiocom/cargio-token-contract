ee8c7565272cf19fe1cbe7d36b6458d4e3ac4fa0
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-09-04 08:02:20 -0700

    Update Cargio Token.sol

--- Cargio-token-contract/Cargio Token.sol
+++ Cargio-token-contract/Cargio Token.sol
@@ -36,7 +36,7 @@ contract Context {
   }
 
   function _msgData() internal view returns (bytes memory) {
-    this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+    this; 
     return msg.data;
   }
 }
