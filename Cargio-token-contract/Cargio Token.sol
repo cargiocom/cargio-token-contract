9bba970c11055ebaf1adec649f909f818dc636cc
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-09-04 08:02:00 -0700

    Update Cargio Token.sol

--- Cargio-token-contract/Cargio Token.sol
+++ Cargio-token-contract/Cargio Token.sol
@@ -29,8 +29,6 @@ interface IBEP20 {
 }
 
 contract Context {
-  // Empty internal constructor, to prevent people from mistakenly deploying
-  // an instance of this contract, which should be used via inheritance.
   constructor () { }
 
   function _msgSender() internal view returns (address) {
