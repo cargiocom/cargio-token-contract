d4a6580116a37e3ae65d8b3111782df94afd3d51
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-09-04 08:02:53 -0700

    Update Cargio Token.sol

--- Cargio-token-contract/Cargio Token.sol
+++ Cargio-token-contract/Cargio Token.sol
@@ -42,15 +42,6 @@ contract Context {
 }
 
 library SafeMath {
-  /**
-   * @dev Returns the addition of two unsigned integers, reverting on
-   * overflow.
-   *
-   * Counterpart to Solidity's `+` operator.
-   *
-   * Requirements:
-   * - Addition cannot overflow.
-   */
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     require(c >= a, "SafeMath: addition overflow");
