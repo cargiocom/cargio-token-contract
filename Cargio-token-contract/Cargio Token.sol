655badf64752ce0686e72de4104acd1911b86e2e
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-09-04 08:00:21 -0700

    Update Cargio Token.sol

--- Cargio-token-contract/Cargio Token.sol
+++ Cargio-token-contract/Cargio Token.sol
@@ -15,13 +15,6 @@ interface IBEP20 {
 
   function balanceOf(address account) external view returns (uint256);
 
-  /**
-   * @dev Moves `amount` tokens from the caller's account to `recipient`.
-   *
-   * Returns a boolean value indicating whether the operation succeeded.
-   *
-   * Emits a {Transfer} event.
-   */
   function transfer(address recipient, uint256 amount) external returns (bool);
 
   /**
