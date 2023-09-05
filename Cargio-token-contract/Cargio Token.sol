9f4082a370fb9ae6878850b659fa4f21022ac6be
Cargio <141646415+cargiocom@users.noreply.github.com>
2023-09-05 02:54:37 -0700

    Update Cargio Token.sol

--- Cargio-token-contract/Cargio Token.sol
+++ Cargio-token-contract/Cargio Token.sol
@@ -106,30 +106,16 @@ contract Ownable is Context {
     return _owner;
   }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
   modifier onlyOwner() {
     require(_owner == _msgSender(), "Ownable: caller is not the owner");
     _;
   }
 
-  /**
-   * @dev Leaves the contract without owner. It will not be possible to call
-   * `onlyOwner` functions anymore. Can only be called by the current owner.
-   *
-   * NOTE: Renouncing ownership will leave the contract without an owner,
-   * thereby removing any functionality that is only available to the owner.
-   */
   function renounceOwnership() public onlyOwner {
     emit OwnershipTransferred(_owner, address(0));
     _owner = address(0);
   }
 
-  /**
-   * @dev Transfers ownership of the contract to a new account (`newOwner`).
-   * Can only be called by the current owner.
-   */
   function transferOwnership(address newOwner) public onlyOwner {
     _transferOwnership(newOwner);
   }
